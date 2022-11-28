778bb46e13c08b28c9de0167851d584c58a1f9de
Филип Ташкоски <93309577+ftaskoski123@users.noreply.github.com>
2022-11-28 10:21:25 +0100

    Add files via upload

---
+++ Guess-My-Number/script.js
@@ -0,0 +1,86 @@
+'use strict';
+
+let number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  //when there is no input
+  if (!guess) {
+    // document.querySelector('.message').textContent = 'No Number';
+    displayMessage('No Number');
+    document.querySelector('.left').classList.add('hidden');
+
+    //When player wins
+  } else if (guess === number) {
+    // document.querySelector('.message').textContent = 'Correct Number!';
+    displayMessage('Correct Number');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').textContent = number;
+    document.querySelector('.number').style.width = '30rem';
+
+    //Updating highscore
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    //When guess is wrong
+  } else if (guess !== number) {
+    if (score > 1) {
+      // document.querySelector('.message').textContent =
+      //   guess > number ? 'Too High' : 'Too Low';
+      displayMessage(guess > number ? 'Too High' : 'Too Low');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      // document.querySelector('.message').textContent = 'You lost the game';
+      displayMessage('You lost the game');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+
+  //Old Code
+  //   //when guess is too high
+  // } else if (guess > number) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = 'Too High';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = 'You lost the game';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+
+  //   //when guess is too low
+  // } else if (guess < number) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = 'Too Low';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = 'You lost the game';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // }
+});
+
+//The again button
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  number = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.left').classList.remove('hidden');
+});
